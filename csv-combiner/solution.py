6b0924a2458b4340926c92fc4e24627b5fa52558
Atu175 <59740296+Atu175@users.noreply.github.com>
2022-11-14 13:54:42 -0500

    Finished project

---
+++ csv-combiner/solution.py
@@ -0,0 +1,18 @@
+import pandas
+import sys
+p = sys.argv[0]
+x = sys.argv[1]
+y = sys.argv[2]
+print(p)
+print(x)
+print(y)
+#x = "fixtures/accessories.csv"
+#y = "fixtures/clothing.csv"
+pathx = x.split("/")
+pathy = y.split("/")
+df1 = pandas.read_csv(x)
+df1.insert(2, 'filename', pathx[1])
+df2 = pandas.read_csv(y)
+df2.insert(2, 'filename', pathy[1])
+merged = pandas.concat([df1, df2])
+merged.to_csv("answer.csv")
